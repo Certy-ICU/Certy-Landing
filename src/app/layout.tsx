f52f30d393f462f22ba3a71bde96673b1b201368
Mitch <Mitch.Ngugi99@gmail.com>
2023-10-24 00:07:00 +0300

    Updated Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,9 @@ import { type Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - TaxPal',
-    default: 'TaxPal - Accounting made simple for small businesses',
+    template: '%s - Certy ICU',
+    default:
+      'Certy ICU - Empowering learners through expert training and certification.',
   },
   description:
     'Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited.',
